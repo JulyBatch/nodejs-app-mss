20789ee03e8fd1ae2b7a6aff692972675584d53a
MithunTechnolog <107618921+MithunTechnolog@users.noreply.github.com>
2022-07-24 21:42:45 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': '8cc5903f1a3b7ec268634aaf609048c2214e8e42',
+	    'sonar.login': '652d6d07f51ff1a74274c5a9901ac70c3caa6d20',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
